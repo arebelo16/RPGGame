4efe60417011334ab856734880910d025f3aeac3
arebelo16 <andre.rebelo16@gmail.com>
2024-03-11 01:52:33 +0000

    Using Set By Caller Damage

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -85,27 +85,28 @@ FString("Amount of mana obtainable"));
 	* Input Tags
 	 */
 	
-	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get()
-		.AddNativeGameplayTag(FName("InputTag.LMB"),
+	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.LMB"),
 FString("Input tag for Left Mouse Button"));
 
-	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get()
-	.AddNativeGameplayTag(FName("InputTag.RMB"),
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.RMB"),
 FString("Input tag for Right Mouse Button"));
 	
-	GameplayTags.InputTag_1 = UGameplayTagsManager::Get()
-	.AddNativeGameplayTag(FName("InputTag.1"),
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.1"),
 FString("Input tag for number 1 key"));
 		
-	GameplayTags.InputTag_2 = UGameplayTagsManager::Get()
-	.AddNativeGameplayTag(FName("InputTag.2"),
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.2"),
 FString("Input tag for number 2 key"));
 		
-	GameplayTags.InputTag_3 = UGameplayTagsManager::Get()
-	.AddNativeGameplayTag(FName("InputTag.3"),
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.3"),
 FString("Input tag for number 3 key"));
 		
-	GameplayTags.InputTag_4 = UGameplayTagsManager::Get()
-	.AddNativeGameplayTag(FName("InputTag.4"),
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.4"),
 FString("Input tag for number 4 key"));
+
+	/*
+	 * Damage
+	 */
+
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Damage"),
+FString("Damage"));
 }
